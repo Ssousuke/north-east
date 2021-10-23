21362767753c4d6b447312a8c7cecd5d6ed579d9
Ssousuke <wesleysousukee@gmail.com>
2021-10-23 18:00:26 -0300

    adiciona path de arquivos estaticos

--- onfire/urls.py
+++ onfire/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('backend.publication.urls', namespace='publication'))
-]
+    path('', include('backend.publication.urls', namespace='publication')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
