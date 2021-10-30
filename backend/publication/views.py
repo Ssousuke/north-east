125ad275bf81de11984f00d5a5f0682b85092fdf
Ssousuke <wesleysousukee@gmail.com>
2021-10-30 17:22:11 -0300

    implementa view para listagem de categorias e listagem de posts por categoria

--- backend/publication/views.py
+++ backend/publication/views.py
@@ -1,21 +1,12 @@
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
-from backend.publication.models import Post
+from backend.publication.models import Post, Category
 
 
 def home_page(request):
-    """
-        :template_name = recebe o html a serem renderizado
-        :publication = recebe os objetos a serem renderizados no html
-        :paginator = recebe os objetos e a quantidade que vão ser renderizados
-        : 
-
-    """
     template_name = 'pages/index.html'
-
     publication = Post.objects.all()
     paginator = Paginator(publication, 16)
-
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {
@@ -30,4 +21,23 @@ def detail(request, slug):
     context = {
         'detail': detail,
     }
+    return render(request, template_name, context)
+
+
+def category(request):
+    category = Category.objects.all()
+    template_name = 'pages/category.html'
+    context = {
+        'category': category
+    }
+    return render(request, template_name, context)
+
+
+def post_category(request, category):
+    template_name = 'pages/index.html'
+    page_obj = Post.objects.filter(category__category__iexact=category)
+    context = {
+        'page_obj': page_obj,
+    }
+    print(context)
     return render(request, template_name, context)
