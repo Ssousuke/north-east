393b02ed2d6ebd826740a09d5d2bf3ef86db6973
Ssousuke <wesleysousukee@gmail.com>
2021-11-03 01:45:15 -0300

    runtime e static_root

--- onfire/settings.py
+++ onfire/settings.py
@@ -111,7 +111,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [BASE_DIR, 'frontend/static']
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'frontend/staticfiles')
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
