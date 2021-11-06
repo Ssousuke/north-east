a6d755d243c8bdd57a0e7447aca65f64c3cff24f
Ssousuke <wesleysousukee@gmail.com>
2021-11-06 20:41:49 -0300

    ajusta filtro

--- backend/publication/views.py
+++ backend/publication/views.py
@@ -48,9 +48,9 @@ def search(request):
     if request.method == 'POST':
         searched = request.POST['searched']
         result_list = Post.objects.filter(
-            Q(title__contains=searched) |
-            Q(description__contains=searched) |
-            Q(category__category__contains=searched)
+            Q(title__iexact=searched) |
+            Q(description__iexact=searched) |
+            Q(category__category__iexact=searched)
         )
         context = {
             'searched': searched,
