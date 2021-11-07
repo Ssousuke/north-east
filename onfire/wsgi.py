7cf5e3a9d3e478bdb6199ac55e18da62007c7afb
Ssousuke <wesleysousukee@gmail.com>
2021-11-06 21:08:07 -0300

    fix heroku

--- onfire/wsgi.py
+++ onfire/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'info_series.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onfire.settings')
 
 application = get_wsgi_application()
